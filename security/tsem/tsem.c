24b4a19e3c95dec9e9019423b7f11ad2d0c61499
Dr. Greg <greg@enjellic.com>
2023-11-25 04:18:45 -0600

    Implement explicit modeling of inode_setattr security hook.
    The commit converts the TSEM implemention of the inode_setattr
    event handler from generic to explicit modeling.
    
    The characteristics of the inode to be changed are specified by
    the iattr structure that uses the valid member to indicate what
    components of the structure have valid information to be changed.
    In order to obtain consistent state values all members of iattr
    structure that characterize the event are set to zero, except for
    those indicated to be valid.
    
    A new strategy is entertained in this commit for handling the
    parameters for the event that will be expanded in use as new
    evenets are fully modeled.
    
    The tsem_inode_setattr_args structure uses an anonymous union to
    hold two structures named in and out.  The in structure contains
    pointers to the arguments supplied to the tsem_inode_setattr
    handler while the out structure contains data elements from the
    input structure that are maintained for the lifetime of the
    security event description.
    
    Since a dentry structure is used to specify the object whose
    attributes are to be set, a new function was in the event.c file
    that creates a path description given a dentry structure.

--- security/tsem/tsem.c
+++ security/tsem/tsem.c
@@ -1583,7 +1583,11 @@ static int tsem_inode_mknod(struct inode *dir, struct dentry *dentry,
 
 static int tsem_inode_setattr(struct dentry *dentry, struct iattr *attr)
 {
+	int retn = 0;
 	char msg[TRAPPED_MSG_LENGTH];
+	struct tsem_event *ep = NULL;
+	struct tsem_inode_setattr_args args;
+	struct tsem_event_parameters params;
 
 	if (unlikely(!tsem_ready))
 		return 0;
@@ -1598,7 +1602,24 @@ static int tsem_inode_setattr(struct dentry *dentry, struct iattr *attr)
 		return trapped_task(TSEM_INODE_SETATTR, msg, NOLOCK);
 	}
 
-	return model_generic_event(TSEM_INODE_SETATTR, NOLOCK);
+	if (bypass_filesystem(dentry->d_inode))
+		return 0;
+
+	args.in.dentry = dentry;
+	args.in.iattr = attr;
+	params.u.inode_setattr = &args;
+
+	ep = tsem_map_event(TSEM_INODE_SETATTR, &params);
+	if (IS_ERR(ep)) {
+		retn = PTR_ERR(ep);
+		goto done;
+	}
+
+	retn = model_event(ep);
+	tsem_event_put(ep);
+
+ done:
+	return retn;
 }
 
 static int tsem_inode_getattr(const struct path *path)
