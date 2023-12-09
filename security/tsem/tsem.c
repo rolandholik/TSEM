996b9332840c2b71b9ed6476597b87f747dff2fd
Dr. Greg <greg@enjellic.com>
2023-12-09 10:57:15 -0600

    Convert inode attribute handlers to common parameter management.
    Following in the theme of the work that was done on the handlers
    that manage extended attributes the input and retention arguments
    for the tsem_inode_getattr and tsem_inode_setattr handlers were
    converted to use a single structure that was named as follows:
    
    tsem_inode_attr_args
    
    The freeing of the retained arguments was consolidated into a
    single switch case in the free_cell() function.

--- security/tsem/tsem.c
+++ security/tsem/tsem.c
@@ -1598,7 +1598,7 @@ static int tsem_inode_setattr(struct dentry *dentry, struct iattr *attr)
 	int retn = 0;
 	char msg[TRAPPED_MSG_LENGTH];
 	struct tsem_event *ep = NULL;
-	struct tsem_inode_setattr_args args;
+	struct tsem_inode_attr_args args;
 	struct tsem_event_parameters params;
 
 	if (unlikely(!tsem_ready))
@@ -1619,7 +1619,7 @@ static int tsem_inode_setattr(struct dentry *dentry, struct iattr *attr)
 
 	args.in.dentry = dentry;
 	args.in.iattr = attr;
-	params.u.inode_setattr = &args;
+	params.u.inode_attr = &args;
 
 	ep = tsem_map_event(TSEM_INODE_SETATTR, &params);
 	if (IS_ERR(ep)) {
@@ -1639,7 +1639,7 @@ static int tsem_inode_getattr(const struct path *path)
 	int retn = 0;
 	char msg[TRAPPED_MSG_LENGTH];
 	struct tsem_event *ep = NULL;
-	struct tsem_inode_getattr_args args;
+	struct tsem_inode_attr_args args;
 	struct tsem_event_parameters params;
 
 	if (unlikely(!tsem_ready))
@@ -1654,8 +1654,8 @@ static int tsem_inode_getattr(const struct path *path)
 	if (bypass_filesystem(path->dentry->d_inode))
 		return 0;
 
-	args.path_arg = path;
-	params.u.inode_getattr = &args;
+	args.in.path = path;
+	params.u.inode_attr = &args;
 	ep = tsem_map_event(TSEM_INODE_GETATTR, &params);
 	if (IS_ERR(ep)) {
 		retn = PTR_ERR(ep);
