0803c9fb6371d796a08aa7b85bea3abc5e953a54
Dr. Greg <greg@enjellic.com>
2023-12-21 10:34:51 -0600

    Fully parameterize the tsem_file_lock handler.
    The conversion of the handler for the file_lock LSM hook was
    largely straight forward and was able to reuse the infrastructure
    from the tsem_file_ioctl handler.
    
    The most notable change was to add code in the
    event.c:get_file_cell() function to bypass the collection of the
    file digest if the inode does not represent a 'regular' file.
    
    The tsem_file_lock() handler was returning an error if the lock
    was executed against a directory secondary to a failure caused by
    trying to obtain the checksum of a directory.

--- security/tsem/fs.c
+++ security/tsem/fs.c
@@ -1374,6 +1374,7 @@ void tsem_fs_show_trajectory(struct seq_file *c, struct tsem_event *ep)
 		show_mmap(c, ep);
 		break;
 	case TSEM_FILE_IOCTL:
+	case TSEM_FILE_LOCK:
 		show_file_ioctl(c, ep);
 		break;
 	case TSEM_SOCKET_CREATE:
