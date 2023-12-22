607d93d70e9bab277feee953dab36525409fd910
Dr. Greg <greg@enjellic.com>
2023-12-22 17:32:05 -0600

    Fully parameterize tsem_task_getioprio handler.
    This handler was parameterized by re-using the infrastructure in
    place for the TSEM_TASK_GETPGID and TSEM_TASK_GETSID handlers.

--- security/tsem/map.c
+++ security/tsem/map.c
@@ -575,6 +575,7 @@ static int get_cell_mapping(struct tsem_event *ep, u8 *mapping)
 
 	case TSEM_TASK_GETPGID:
 	case TSEM_TASK_GETSID:
+	case TSEM_TASK_GETIOPRIO:
 		p = ep->CELL.task_kill.target;
 		size = sizeof(ep->CELL.task_kill.target);
 		retn = crypto_shash_finup(shash, p, size, mapping);
