646d51b4d1b5e2cb1c5851e4bb502959affbcd35
Dr. Greg <greg@enjellic.com>
2024-01-20 13:50:07 -0600

    Convert tsem_sb_statfs to use dentry infrastructure.
    The conversion of this security handler to use the dentry
    structure in the 'out' structure of the tsem_sb_args structure.
    
    The event.c:get_sb_statfs() function was updated to use the
    fill_dentry() function to transfer the dentry argument of the
    handler to its tsem_dentry equivalent.  The free_cell() function
    was updated to release memory from the path structure that is in
    the tsem_dentry structure.
    
    All that was needed in the map.c file was call the add_dentry()
    in the clause in the switch statement that is handling the
    TSEM_SB_STATFS event.
    
    In the fs.c:show_sb_statfs() function the show_dentry() function
    was used to display description for the event.

--- security/tsem/map.c
+++ security/tsem/map.c
@@ -1233,11 +1233,7 @@ static int get_cell_mapping(struct tsem_event *ep, u8 *mapping)
 		break;
 
 	case TSEM_SB_STATFS:
-		retn = add_path(shash, &ep->CELL.sb.out.path);
-		if (retn)
-			goto done;
-
-		retn = add_inode(shash, &ep->CELL.sb.out.inode);
+		retn = add_dentry(shash, &ep->CELL.sb.out.dentry);
 		if (retn)
 			goto done;
 
