e895f0818e538ebde102d2f4c8ea5ba6faa1e398
Dr. Greg <greg@enjellic.com>
2024-05-30 19:07:48 -0500

    Make security module registration dependent on module support.
    Since alternate security models can only be registered in the
    kernel by loadable module there is no need to have the model
    manager implemented if kernel modules are not enabled.
    
    Make the inclusion of the nsmgr.o object file dependent on
    CONFIG_MODULE.
    
    The globally visible interfaces for module registration were
    moved to the nsmgr.h file so that alternate stub functions can be
    generated provide functional interfaces in the absence of
    loadable modules.

--- security/tsem/tsem.h
+++ security/tsem/tsem.h
@@ -2249,12 +2249,6 @@ extern int tsem_event_cache_init(void);
 extern u8 *tsem_trust_aggregate(void);
 extern int tsem_trust_add_event(struct tsem_event *ep);
 
-extern void tsem_nsmgr_put(const struct tsem_context_ops *ops);
-extern const struct tsem_context_ops *tsem_nsmgr_get(const char *name);
-extern int tsem_nsmgr_register(const struct tsem_context_ops *ops,
-			       struct module *module);
-extern void tsem_nsmgr_release(const struct tsem_context_ops *ops);
-
 /*
  * The remaining inline function declarations follow the design
  * pattern of the other LSM's and implement functions that return
